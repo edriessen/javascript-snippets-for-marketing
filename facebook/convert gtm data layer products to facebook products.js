65da44143effa4fb6783790648a63b7cd9d8c9ad
Erik Driessen <erik.driessen@outlook.com>
2018-11-23 12:10:08 +0100

    Update convert gtm data layer products to facebook products.js

--- facebook/convert gtm data layer products to facebook products.js
+++ facebook/convert gtm data layer products to facebook products.js
@@ -2,14 +2,14 @@ function(){
   var items = {{Data Layer - ecommerce purchase products}}, //change to make it match your GTM variable setup
       fb_items = [];
   for(var i = 0; i < items.length; i++){
-    product_id = items[i].id
+   var product_id = items[i].id
     if (document.location.pathname === '/bedankt-voor-je-bestelling/'){
       product_id = items[i].brand
     }
   	fb_items.push({
-    	id: product_id,
-        quantity: items[i].quantity,
-        item_price: items[i].price
+      id: product_id,
+      quantity: items[i].quantity,
+      item_price: items[i].price
     });
   }
   return fb_items;	
